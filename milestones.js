70824217960eff5fb59b65b4a2e57be98b47837a
Diego F.(EW7) <diegoefe@gmail.com>
2016-11-21 11:38:54 -0300

    Leyendo en storage

--- milestones.js
+++ milestones.js
@@ -7,6 +7,23 @@ if (typeof localStorage === "undefined" || localStorage === null) {
   var LocalStorage = require('node-localstorage').LocalStorage;
   milestones.testDir = testDir.getDir('milestones-storage');
   var localStorage = new LocalStorage(milestones.testDir);
+  milestones.storage = localStorage;
 }
+
+milestones.addUrl = function addUrl(url, headers, rateLimiteExceeded) {
+    var urlData = {headers:headers, rateLimiteExceeded:rateLimiteExceeded};
+    localStorage.setItem(url, JSON.stringify(urlData));
+    return urlData;
+};
+
+milestones.getUrl = function getUrl(url) { return JSON.parse(localStorage.getItem(url)); };
+
+milestones.add = function add(title, proyects, url) {
+    var milestoneData = {proyects:proyects, url:url};
+    localStorage.setItem(title, JSON.stringify(milestoneData));
+    return milestoneData;
+}
+
+milestones.get = function get(title) { return JSON.parse(localStorage.getItem(title)); };
 
 module.exports = milestones;
--- test/test.js
+++ test/test.js
@@ -5,14 +5,16 @@ var fetch = require('node-fetch');
 var milestones = require('../milestones.js');
 var fs = require('fs-promise');
 
+
 before(function(done){
     this.timeout(5000);
     Promise.resolve().then(function() {
         return fs.exists(milestones.testDir);
     }).then(function(existe) {
-        if(existe) { return fs.remove(milestones.testDir); }
+        //if(existe) { return fs.remove(milestones.testDir); }
+        if(! existe) { return fs.mkdirs(milestones.testDir); }
     }).then(function() {
-        return fs.mkdirs(milestones.testDir);
+        //return fs.mkdirs(milestones.testDir);
     }).then(function() {
         done();
     }).catch(function(err){
@@ -20,9 +22,70 @@ before(function(done){
     });
 });
 
+function leerMilestones(storage, page){
+    var page=page||1;
+    var baseUrl = 'https://api.github.com/orgs/codenautas/repos?page='+page; 
+    return fetch(baseUrl)
+    .then(function(response){
+        var headers = response.headers.raw();
+        //console.log("headers", headers);
+        var data = storage.addUrl(baseUrl, headers, headers.status[0].match(/403/));
+        if(data.rateLimiteExceeded) {
+            console.log("Rate-limit exceeded")
+            return false;
+        }
+        return response.json();
+    }).then(function(arr){
+        if(! arr) {
+            return storage;
+        }
+        /* arr=[
+            {name: 'backend-plus'},
+            {name: 'dialog-promise'},
+            {name: 'typed-controls'},
+        ] // */
+        console.log(arr);
+        storage.storage.setItem("resp", JSON.stringify(arr));
+        return Promise.all(
+            arr.map(function(proyecto){
+                var url = 'https://api.github.com/repos/codenautas/'+proyecto.name+'/milestones?state=all';
+                return fetch(url)
+                .then(function(response){
+                    return response.json();
+                }).then(function(arr){
+                    arr.forEach(function(milestone){
+                        var title = milestone.title || 'no-title';
+                        storage.add(title, milestone, url);
+                        //milestones[milestone.title] = milestones[milestone.title] || { proyectos: {} };
+                        //milestones[milestone.title].proyectos[proyecto.name] = milestone;
+                    });
+                });
+            })
+        ).then(function(){
+            if(arr.length /* && false */){
+                return leerMilestones(storage, page+1);
+            }
+            return storage;
+        });
+    });
+}
+
 describe('milestones', function(){
     it('storage', function(done){
+        //console.log(milestones.storage); return done();
+        this.timeout(20000);
         console.log(milestones.testDir);
-        done();
+        leerMilestones(milestones).then(function() {
+            var page1 = milestones.getUrl('https://api.github.com/orgs/codenautas/repos?page=1');
+            if(page1.rateLimiteExceeded) {
+                console.log('Requeset avalability at '+new Date(page1.headers['x-ratelimit-reset'] * 1000));
+            }
+            
+            //console.log(milestones.storage);
+            //console.log("URL", page1);
+            done();
+        }).catch(function(err) {
+            done(err);
+        });
     });
 });
