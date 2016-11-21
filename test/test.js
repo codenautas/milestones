6fd4259e7aa53615d9f92b59cc711a7154b1f02d
Diego F.(EW7) <diegoefe@gmail.com>
2016-11-21 09:38:49 -0300

    Agrego infraestructura de test

---
+++ test/test.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var expect = require('expect.js');
+var fetch = require('node-fetch');
+
+describe('milestones', function(){
+    it('pagination', function(done){
+        done();
+    });
+});
