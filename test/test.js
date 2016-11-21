70551e7c4cb44d466c1ca09c60340b79a2c54be0
Diego F.(EW7) <diegoefe@gmail.com>
2016-11-21 10:25:26 -0300

    Test dir reset

--- test/test.js
+++ test/test.js
@@ -3,6 +3,22 @@
 var expect = require('expect.js');
 var fetch = require('node-fetch');
 var milestones = require('../milestones.js');
+var fs = require('fs-promise');
+
+before(function(done){
+    this.timeout(5000);
+    Promise.resolve().then(function() {
+        return fs.exists(milestones.testDir);
+    }).then(function(existe) {
+        if(existe) { return fs.remove(milestones.testDir); }
+    }).then(function() {
+        return fs.mkdirs(milestones.testDir);
+    }).then(function() {
+        done();
+    }).catch(function(err){
+        console.log(err);
+    });
+});
 
 describe('milestones', function(){
     it('storage', function(done){
