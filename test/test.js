0e31ac1e210792f348e69fa6b4d2c5b2050773a2
Diego F.(EW7) <diegoefe@gmail.com>
2016-11-21 09:52:35 -0300

    Agrego node-localstorage para testear

--- test/test.js
+++ test/test.js
@@ -2,6 +2,7 @@
 
 var expect = require('expect.js');
 var fetch = require('node-fetch');
+var storage = require('node-localstorage').LocalStorage;
 
 describe('milestones', function(){
     it('pagination', function(done){
