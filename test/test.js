787ad165e2a7a9fa1cb517f0faf91718b86436e9
Diego F.(EW7) <diegoefe@gmail.com>
2016-11-22 08:19:31 -0300

    Muevo milestones a app

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 "use strict";
 
 var expect = require('expect.js');
-var milestones = require('../milestones.js');
+var milestones = require('../app/milestones.js');
 var fs = require('fs-promise');
 var sinon = require('sinon');
 
